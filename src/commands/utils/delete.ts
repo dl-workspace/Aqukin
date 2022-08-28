5ddf6e5dcf5e9f12e76760a6f04b12e5adf4546a
Minh Duc Le <dealoux+github@gmail.com>
2022-08-27 19:15:59 -0500

    ui update

--- src/commands/utils/delete.ts
+++ src/commands/utils/delete.ts
@@ -2,15 +2,15 @@ import { ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
 import { Command, COMMAND_TAGS } from "../../structures/Command";
 
 export default new Command({
-    name: 'clean_message',
+    name: 'delete',
     tag: COMMAND_TAGS.info,
-    description: 'Remove a specified number (min 1, default 10, max 100) of messages in the current text channel',
+    description: 'Delete a specified number (min 1, default 10, max 100) of messages in the current text channel',
     userPermissions: [PermissionFlagsBits.Administrator],
     ephemeral: true,
     options: [{
         type: ApplicationCommandOptionType.Number,
         name: 'num',
-        description: 'number of messages to remove (min 1, default 10, max 100)',
+        description: 'number of messages to delete (min 1, default 10, max 100)',
         min_value: 1,
         max_value: 100,
         required: false,
@@ -21,7 +21,7 @@ export default new Command({
         const num = args.get('num')?.value as number || 10;
 
         await channel.bulkDelete(num, true).then(async (deleted) => {
-            interaction.editReply({ content: `**${user.username}**-sama, ${client.user.username} has cleaned \`${deleted.size}\` messages` });
+            interaction.editReply({ content: `**${user.username}**-sama, ${client.user.username} has deleted \`${deleted.size}\` messages` });
         }).catch(err => channel.send({ content: `**${user.username}**-sama, \`${err}\`` }));
     }
 });
