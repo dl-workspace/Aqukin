569ab0a5fb76954a4cf38e16895d911caaa72acd
Minh Duc Le <dealoux+github@gmail.com>
2022-11-03 01:19:45 -0500

    Update play.ts

--- src/commands/opus/play.ts
+++ src/commands/opus/play.ts
@@ -84,7 +84,7 @@ export default new Command({
             
         }
         else if(args.getSubcommand() == PLAY_OPTIONS.queue){
-            result = await processQuery({ client, interaction, args }, mPlayer.queue.length);
+            result = await processQuery({ client, interaction, args }, mPlayer.queue.length || 0);
 
             if(result.length > 0){
                 mPlayer.queue.push(...result);
