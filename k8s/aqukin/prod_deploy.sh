8bd5e25f9ec990f9bc9fc1de3f4efa3f0f325721
VincentB00 <vincentbui1999@gmail.com>
2023-04-20 01:14:02 -0400

    fix k8s deployment

--- k8s/aqukin/prod_deploy.sh
+++ k8s/aqukin/prod_deploy.sh
@@ -1,4 +1,4 @@
-namespace=authenticator-prod
+namespace=aqukin-prod
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f prod_deployment.yaml
--- k8s/aqukin/stage_deploy.sh
+++ k8s/aqukin/stage_deploy.sh
@@ -1,4 +1,4 @@
-namespace=authenticator-stage
+namespace=aqukin-stage
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f stage_deployment.yaml
