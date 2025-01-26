ba536484a56fbdda793632a1dc7275aec5dc4e09
Vincent <74420313+VincentB00@users.noreply.github.com>
2025-01-25 23:46:53 -0500

    Delete k8s directory

--- k8s/aqukin/dev_deploy.sh
+++
@@ -1,5 +0,0 @@
-namespace=aqukin-dev
-microk8s kubectl delete namespace ${namespace}
-microk8s kubectl create namespace ${namespace}
-microk8s kubectl -n ${namespace} apply -f dev_deployment.yaml
-# microk8s kubectl -n ${namespace} apply -f dev_service.yaml
